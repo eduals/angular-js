dc9300236e266a7c17f5eadf07f27a5aac7f2f0e
Danail Kyosev <dkyosev@kleverbeast.com>
2014-07-18 17:34:50 +0300

    Initial appointments

--- app/js/controllers/appointment.js
+++ app/js/controllers/appointment.js
@@ -2,6 +2,14 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers').controller('Appointment', ['$scope', function($scope) {
+angular.module('myApp.controllers').controller('Appointment', ['$scope', '$rootScope', function($scope, $rootScope) {
+	if (!$rootScope.appointments) {
+		$rootScope.appointments = [];
+	}
 
+	$scope.appointment = {};
+
+	$scope.save = function() {
+		$rootScope.appointments.push($scope.appointment);
+	}
 }]);
