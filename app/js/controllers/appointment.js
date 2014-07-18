4617f6c127ce6d354478c1d8235eaaf6468446ce
Danail Kyosev <dkyosev@kleverbeast.com>
2014-07-18 18:00:42 +0300

    Appointment logic

--- app/js/controllers/appointment.js
+++ app/js/controllers/appointment.js
@@ -2,14 +2,20 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers').controller('Appointment', ['$scope', '$rootScope', function($scope, $rootScope) {
+angular.module('myApp.controllers').controller('Appointment', ['$scope', '$rootScope', '$location', 'rooms', function($scope, $rootScope, $location, rooms) {
 	if (!$rootScope.appointments) {
 		$rootScope.appointments = [];
 	}
 
-	$scope.appointment = {};
+	$scope.appointment = {
+		'room': rooms[0],
+		'duration': 30
+	};
+	$scope.rooms = rooms;
 
 	$scope.save = function() {
+		$scope.appointment.date = new Date($scope.appointment.date);
 		$rootScope.appointments.push($scope.appointment);
+		$location.path('/status');
 	}
 }]);
